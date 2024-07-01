a0cc1d6a0050a238aa40572875fc8a761afb9e7a
xxizunoxx <caique_mk2@hotmail.com>
2024-07-01 07:26:46 -0300

    task 4

--- Curso-3/carrinho-compras/js/app.js
+++ Curso-3/carrinho-compras/js/app.js
@@ -1,25 +1,48 @@
 let totalGeral;
+limpar();
 
-function adicionar(){
+
+function adicionar() {
     let produto = document.getElementById('produto').value;
-    let nomeProduto = produto.split('-')[0];
-    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
+
+
+    // Verificar se o produto selecionado é válido
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
+
+
+    // Verificar se a quantidade inserida é válida
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
+
+
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = parseFloat(produto.split('R$')[1]);
     let preco = quantidade * valorUnitario;
+
+
     let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
-        <section class="carrinho__produtos__produto">
-        <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
-        </section>`;
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
+  </section>`;
+
+
     totalGeral = totalGeral + preco;
     let campoTotal = document.getElementById('valor-total');
     campoTotal.textContent = `R$ ${totalGeral}`;
     document.getElementById('quantidade').value = 0;
 }
 
+
+
+
 function limpar() {
     totalGeral = 0;
     document.getElementById('lista-produtos').innerHTML = '';
-    document.getElementById('valor-total').textContent = 'R$ 0';
-
+    document.getElementById('valor-total').textContent = 'R$0';
 }
